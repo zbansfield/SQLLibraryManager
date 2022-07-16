4ff8b1e4038bb0126cd6a1582646979bfc509932
zbansfield <zainab_bansfield@intuit.com>
2022-07-15 18:45:38 -0700

    Added /books route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@ const Book = require('../models').Book;
 
 /* GET home page. */
 router.get('/', async(req, res, next) => {
+  res.redirect('/books')
+  // const allBooks = await Book.findAll()
+  // console.log(allBooks);
+  // res.json(allBooks)
+});
+
+// Set up routes
+
+/* GET books page */
+router.get('/books', async(req, res, next) => {
   const allBooks = await Book.findAll()
-  console.log(allBooks);
-  res.json(allBooks)
+  res.render('layout', {allBooks})
 
 });
 
