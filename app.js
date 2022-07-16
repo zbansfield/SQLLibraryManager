2f9693792ebcad999399769ac5cbc9beff0a5633
zbansfield <zainab_bansfield@intuit.com>
2022-07-15 17:57:26 -0700

    Started creating pug files

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const sequelize = new Sequelize({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,18 +42,16 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not found');
+  err.message = "Page not found";
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.render('page-not-found');
 });
 
 module.exports = app;
